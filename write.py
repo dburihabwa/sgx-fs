9fae23992089841d306244a8c059e7a1ace0a328
Dorian Burihabwa <dorian@burihabwa.com>
2018-04-08 14:19:17 +0200

    Update write script

--- write.py
+++ write.py
@@ -6,26 +6,22 @@ import time
 
 DEFAULT_BLOCKSIZE = 4096
 
-def main(path, size):
-    """
-    Args:
-        path(str): Path to the mount point
-        size(int): Size of the file to write (in bytes)
-    """
+
+def write(path, size):
     if not path or not isinstance(path, str):
         raise ValueError("path argument should be a non-empty string")
     if not isinstance(size, int) or size <= 0:
         raise ValueError("size argument should be a integer greater than 0")
-    if not os.path.isdir(path):
-        error_msg = "{:s} mount point is not a valid directory".format(path)
+    base_directory = os.path.dirname(path)
+    if not os.path.isdir(base_directory):
+        error_msg = "{:s} mount point is not a valid directory".format(base_directory)
         raise ValueError(error_msg)
-    file_to_write = os.path.join(path, "dummy.txt")
     stats = {
         "size": size,
         "blocksize": DEFAULT_BLOCKSIZE,
         "response_times": {}
     }
-    with open(file_to_write, "wb") as handle:
+    with open(path, "wb") as handle:
         for offset in range(0, size, DEFAULT_BLOCKSIZE):
             data = os.urandom(DEFAULT_BLOCKSIZE)
             start = time.time()
@@ -33,13 +29,18 @@ def main(path, size):
             end = time.time()
             elapsed = end - start
             stats["response_times"][offset] = elapsed
-    return stats
+        return stats
 
 if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(__file__)
+    PARSER = argparse.ArgumentParser(__file__, description="A script to write one or mulitple files to a fuse mount point for testing purposes")
     PARSER.add_argument("path", help="Path to the mount point", type=str)
+    PARSER.add_argument("count", help="Number of files to write", type=int, default=1)
     PARSER.add_argument("size", help="Size of the file to write (in bytes)", type=int)
     ARGS = PARSER.parse_args()
-    STATS = main(ARGS.path, ARGS.size)
-    print(json.dumps(STATS, indent=4, sort_keys=True))
-    
+    NAME_TEMPLATE = os.path.join(ARGS.path, "dummy-{:d}.txt")
+    RESPONSE_TIMES = {}
+    for index in range(ARGS.count):
+        filepath = NAME_TEMPLATE.format(index + 1)
+        RESPONSE_TIMES[filepath] = write(filepath, ARGS.size)
+    print(json.dumps(RESPONSE_TIMES, indent=4, sort_keys=True))
+            
