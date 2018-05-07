241a211dcf0632d7e6b1194931d5432bd12f3d9f
Dorian Burihabwa <dorian@burihabwa.com>
2018-05-07 17:18:26 +0200

    Cleanup old files

--- start.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-make clean -s
-make SGX_MODE=HW DEBUG=1 SGX_PRERELEASE=1 -s
-mkdir -p /tmp/mnt/fuse
-sudo umount -f /tmp/mnt/fuse
-#nohup ./app-sgx-ramfs -f /tmp/mnt/fuse > fs.log 2>&1 &
-./app-sgx-ramfs /tmp/mnt/fuse
-
