c55469afa5056b2aa96a0a6c5d49409efe4a477b
Dorian Burihabwa <dorian@burihabwa.com>
2018-06-15 15:14:06 +0200

    Update install script
    Update install script to completely rebuild the SGX driver, SDK and PSW
    whenever it is invoked.

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 install_dependencies() {
     # Install SGX driver
     apt-get install --yes --quiet autoconf automake build-essential g++ git libcurl4-openssl-dev libfuse-dev libprotobuf-dev libssl-dev libtool "linux-headers-$(uname -r)" ocaml pkg-config protobuf-compiler python wget &&\
-    git clone https://github.com/intel/linux-sgx-driver /opt/linux-sgx-driver
+    if [[ ! -d /opt/linux-sgx-driver ]]; then
+	    git clone https://github.com/intel/linux-sgx-driver /opt/linux-sgx-driver
+    fi
     cd /opt/linux-sgx-driver
+    git clean -df
     make
     mkdir -p "/lib/modules/$(uname -r)/kernel/drivers/intel/sgx"
     cp isgx.ko "/lib/modules/$(uname -r)/kernel/drivers/intel/sgx"
@@ -14,18 +17,20 @@ install_dependencies() {
 
 
     # Install SGX SDK and PSW
-    apt-get update &&\
-    apt-get dist-upgrade --yes --quiet &&\
-    git clone https://github.com/intel/linux-sgx /opt/linux-sgx &&\
-    cd /opt/linux-sgx &&\
-    git checkout sgx_2.1.2 &&\
-    ./download_prebuilt.sh &&\
-    make sdk_install_pkg &&\
-    make psw_install_pkg &&\
-    cd linux/installer/bin &&\
-    echo "yes" | ./sgx_linux_x64_sdk_2.1.102.43402.bin &&\
-    ./sgx_linux_x64_psw_2.1.102.43402.bin &&\
-    source /opt/linux-sgx/linux/installer/bin/sgxsdk/environment  &&\
+    apt-get update && apt-get dist-upgrade --yes --quiet && apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev --yes --quiet
+    if [[ ! -d /opt/linux-sgx ]]; then
+         git clone https://github.com/intel/linux-sgx /opt/linux-sgx
+    fi
+    cd /opt/linux-sgx
+    git clean -df
+    git checkout sgx_2.1.2
+    ./download_prebuilt.sh
+    make sdk_install_pkg
+    make psw_install_pkg
+    cd linux/installer/bin
+    echo "yes" | ./sgx_linux_x64_sdk_2.1.102.43402.bin
+    ./sgx_linux_x64_psw_2.1.102.43402.bin
+    source /opt/linux-sgx/linux/installer/bin/sgxsdk/environment
     echo -e "\n##### Intel SGX SDK\nsource /opt/linux-sgx/linux/installer/bin/sgxsdk/environment\n" >> ~/.bashrc
 }
 
